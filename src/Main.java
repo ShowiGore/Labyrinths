74fd9da59699bd1d8d0588afcbb654e497d06b99
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 13:13:34 +0200

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import java.util.StringJoiner;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        int height, width;
+
+        height = width = 5000;
+
         Test t1 = new Test();
         t1.start();
-        Labyrinth l1 = new RecursiveDivision(10000,10000);
+        Labyrinth l1 = new RecursiveDivision(height,width);
         t1.end();
         System.out.println("RecursiveDivision:");
         t1.print();
@@ -14,10 +16,11 @@ public class Main {
 
         Test t2 = new Test();
         t2.start();
-        Labyrinth_IntArray l2 = new RecursiveDivision_IntArray(10000,10000);
+        Labyrinth_IntArray l2 = new RecursiveDivision_IntArray(height,width);
         t2.end();
         System.out.println("RecursiveDivision(int):");
         t2.print();
+
 
         //l1.exportPNG();
         //Labyrinth_IntArray l2 = new RecursiveDivision_IntArray(10000,10000);
