79db5f898ccb3db18beed497d52704744ff42e6a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 12:27:30 +0200

    Test class added

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        Long startTime, endTime, elapsedTime, beforeUsedMem, afterUsedMem, actualMemUsed;
-
-        beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        startTime = System.nanoTime();
-
+        Test t1 = new Test();
+        t1.start();
         Labyrinth l1 = new RecursiveDivision(10000,10000);
+        t1.end();
+        System.out.println("RecursiveDivision:");
+        t1.print();
+
+
+        Test t2 = new Test();
+        t2.start();
+        Labyrinth_IntArray l2 = new RecursiveDivision_IntArray(10000,10000);
+        t2.end();
+        System.out.println("RecursiveDivision(int):");
+        t2.print();
+
         //l1.exportPNG();
         //Labyrinth_IntArray l2 = new RecursiveDivision_IntArray(10000,10000);
         //l2.PNG("maze");
 
-        endTime = System.nanoTime();
-        afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
-        elapsedTime = endTime-startTime;
-        actualMemUsed = afterUsedMem - beforeUsedMem;
-
-
-        System.out.println("Time:\t\t"+showTime(elapsedTime));
-        System.out.println("Memory:\t\t"+showMemory(actualMemUsed));
 
         //System.out.println("Seed: " + l1.getSeed());
 
@@ -35,62 +35,7 @@ public class Main {
 
 
         //System.out.println(l1.toString());
-        //System.out.println(l1.toString2())
-
-    }
-
-    public static String showTime(long t) {
-
-        long nanoseconds = t%1000;
-        t = t/1000;
-        long microseconds = t%1000;
-        t = t/1000;
-        long milliseconds = t%1000;
-        t = t/1000;
-        long seconds = t%60;
-        t = t/60;
-        long minutes = t%60;
-        t = t/60;
-        long hours = t%24;
-        t = t/24;
-        long days = t;
-
-        StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
-
-        if (days>0) {sj.add(days+" d");}
-        if (hours>0) {sj.add(hours+" h");}
-        if (minutes>0) {sj.add(minutes+" min");}
-        if (seconds>0) {sj.add(seconds+" s");}
-        if (milliseconds>0) {sj.add(milliseconds+" ms");}
-        if (microseconds>0) {sj.add(microseconds+" µs");}
-        if (nanoseconds>0) {sj.add(nanoseconds+" ns");}
-
-        return(sj.toString());
-
-    }
-
-    public static String showMemory(long m) {
-
-        long bytes = m%1024;
-        m = m/1024;
-        long kibibytes = m%1024;
-        m = m/1024;
-        long mebibytes = m%1024;
-        m = m/1024;
-        long gibibytes = m%1024;
-        m = m/1024;
-        long tebibytes = m%1024;
-        m = m/1024;
-
-        StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
-
-        if (tebibytes>0) {sj.add(tebibytes+" TB");}
-        if (gibibytes>0) {sj.add(gibibytes+" GB");}
-        if (mebibytes>0) {sj.add(mebibytes+" MB");}
-        if (kibibytes>0) {sj.add(kibibytes+" KB");}
-        if (bytes>0) {sj.add(bytes+" B");}
-
-        return(sj.toString());
+        //System.out.println(l1.toString2());
 
     }
 
---
+++ src/Test.java
@@ -0,0 +1,90 @@
+import java.util.StringJoiner;
+
+public class Test {
+
+    Long startTime, endTime, elapsedTime, startMemory, endMemory, usedMemory;
+
+    Test() {}
+
+    public void start() {
+        startMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        startTime = System.nanoTime();
+    }
+
+    public void end() {
+        endTime = System.nanoTime();
+        endMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        elapsedTime = endTime-startTime;
+        usedMemory = endMemory - startMemory;
+    }
+
+    public String getTime() {
+        return showTime(elapsedTime);
+    }
+
+    public String getMemory() {
+        return showMemory(usedMemory);
+    }
+
+    public void print() {
+        System.out.println("Time:\t\t"+showTime(elapsedTime));
+        System.out.println("Memory:\t\t"+showMemory(usedMemory));
+    }
+
+    public static String showTime(long t) {
+
+        long nanoseconds = t%1000;
+        t = t/1000;
+        long microseconds = t%1000;
+        t = t/1000;
+        long milliseconds = t%1000;
+        t = t/1000;
+        long seconds = t%60;
+        t = t/60;
+        long minutes = t%60;
+        t = t/60;
+        long hours = t%24;
+        t = t/24;
+        long days = t;
+
+        StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
+
+        if (days>0) {sj.add(days+" d");}
+        if (hours>0) {sj.add(hours+" h");}
+        if (minutes>0) {sj.add(minutes+" min");}
+        if (seconds>0) {sj.add(seconds+" s");}
+        if (milliseconds>0) {sj.add(milliseconds+" ms");}
+        if (microseconds>0) {sj.add(microseconds+" µs");}
+        if (nanoseconds>0) {sj.add(nanoseconds+" ns");}
+
+        return(sj.toString());
+
+    }
+
+    public static String showMemory(long m) {
+
+        long bytes = m%1024;
+        m = m/1024;
+        long kibibytes = m%1024;
+        m = m/1024;
+        long mebibytes = m%1024;
+        m = m/1024;
+        long gibibytes = m%1024;
+        m = m/1024;
+        long tebibytes = m%1024;
+        m = m/1024;
+
+        StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
+
+        if (tebibytes>0) {sj.add(tebibytes+" TB");}
+        if (gibibytes>0) {sj.add(gibibytes+" GB");}
+        if (mebibytes>0) {sj.add(mebibytes+" MB");}
+        if (kibibytes>0) {sj.add(kibibytes+" KB");}
+        if (bytes>0) {sj.add(bytes+" B");}
+
+        return(sj.toString());
+
+    }
+
+}
